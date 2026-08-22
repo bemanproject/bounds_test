ca7fd52d6563716688b70a01882fe6b95fa7c6bb
Claus Klein <claus.klein@arcormail.de>
2026-08-22 20:53:06 +0200

    Add infra beman submodule and used it (#23)

--- cmake/check_plat.cmake
+++ cmake/check_plat.cmake
@@ -1,20 +1,22 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 function(check_plat HAS_GNU_VAR HAS_MSVC_VAR)
-  include(CheckSourceCompiles)
-  set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+    include(CheckSourceCompiles)
+    set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-  check_source_compiles(CXX
-  "void f() {
+    check_source_compiles(
+        CXX
+        "void f() {
     int a, b, c;
     __builtin_add_overflow(a, b, &c);
     __builtin_sub_overflow(a, b, &c);
     __builtin_mul_overflow(a, b, &c);
   }"
-  HAS_GNU_OVERFLOW
-  )
+        HAS_GNU_OVERFLOW
+    )
 
-  check_source_compiles(CXX
-  "
+    check_source_compiles(
+        CXX
+        "
   #include <intrin.h>
   #include <cstdint>
   void f() {
@@ -22,9 +24,9 @@ function(check_plat HAS_GNU_VAR HAS_MSVC_VAR)
     _add_overflow_i32(0, a, b, &c);
     _sub_overflow_i32(0, a, b, &c);
   }"
-  HAS_MSVC_OVERFLOW
-  )
+        HAS_MSVC_OVERFLOW
+    )
 
-  set(${HAS_GNU_VAR} ${HAS_GNU_OVERFLOW} PARENT_SCOPE)
-  set(${HAS_MSVC_VAR} ${HAS_MSVC_OVERFLOW} PARENT_SCOPE)
+    set(${HAS_GNU_VAR} ${HAS_GNU_OVERFLOW} PARENT_SCOPE)
+    set(${HAS_MSVC_VAR} ${HAS_MSVC_OVERFLOW} PARENT_SCOPE)
 endfunction()
